d99aa996ea593f2144605cda926145b7c10b6974
JBaptista <jbaptista.macosso@gmail.com>
2024-12-28 21:38:21 +0100

    testes

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // URLs base para diferentes ambientes
-export const API_BASE_URL = 'https://sistema-transporte-backend.vercel.app/api';
-export const API_LOCAL = 'http://localhost:3050/api';
+const API_BASE_URL = 'https://sistema-transporte-backend.vercel.app/api';
+//const API_LOCAL = 'http://localhost:3050/api';
 
 // Configuração padrão do Axios
 const api = axios.create({
